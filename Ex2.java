a4295689b125333574f2eed7d245fe8176620458
seungchae <80873640+seungchae1@users.noreply.github.com>
2022-03-30 08:35:11 +0900

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,9 @@
+public class Ex2{
+	public static void main(String  [] args){
+		System.out.println("안녕하세요");
+		System.out.println("제 이름은 박승채입니다");
+		System.out.println("저는 미림여자정보과학고등학교에 다닙니다");
+		System.out.println("소프트웨어과이고요");
+		System.out.print("1학년 2반입니다");
+	}
+}
