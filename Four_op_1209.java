a7b3a90d0aa53bca2d15d4d44bd9cf2f1dcc9a04
seungchae <80873640+seungchae1@users.noreply.github.com>
2022-03-30 09:19:33 +0900

    Add files via upload

---
+++ Four_op_1209.java
@@ -0,0 +1,14 @@
+public class Four_op_1209 {
+	public static void main(String args[]){
+		double a = 3.5;
+		double b = 7.2;
+		System.out.print(a+"+"+b+"= ");
+		System.out.println(a+b);
+		System.out.print(a+"-"+b+"= ");
+		System.out.println(a-b);
+		System.out.print(a+"*"+b+"= ");
+		System.out.println(a*b);
+		System.out.print(a+"/"+b+"= ");
+		System.out.println(a/b);
+	}
+}
