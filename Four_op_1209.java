c07b4daf13023bbf2a5ed3c33febabd26d1c4235
seungchae <80873640+seungchae1@users.noreply.github.com>
2022-03-30 09:21:03 +0900

    Add files via upload
    scanner사용

--- Four_op_1209.java
+++ Four_op_1209.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 public class Four_op_1209 {
 	public static void main(String args[]){
-		double a = 3.5;
-		double b = 7.2;
+		Scanner scan=new Scanner(System.in);
+		double a =scan.nextDouble();
+		double b =scan.nextDouble();
 		System.out.print(a+"+"+b+"= ");
 		System.out.println(a+b);
 		System.out.print(a+"-"+b+"= ");
